6dd135a479255e27a2a5ceb6c68a68f949748e03
Nenad Marinkovic <nenadmarinkovic@protonmail.com>
2020-09-02 14:48:53 +0200

    react helmet add

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,18 +21,20 @@ const SEOKeywords = [
 
 export default () => {
   return (
-    <Layout>
+    <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>
           Reuse | Reusable code snippets for web developers and designers.
         </title>
       </Helmet>
-      <SEO
-        title="Reusable code snippets for web developers and designers."
-        description={SEODescription}
-        keywords={SEOKeywords}
-      />
-    </Layout>
+      <Layout>
+        <SEO
+          title="Reusable code snippets for web developers and designers."
+          description={SEODescription}
+          keywords={SEOKeywords}
+        />
+      </Layout>
+    </>
   );
 };
